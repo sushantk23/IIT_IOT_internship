05b7aa0f9d11f94b932dcff7d05adb976e640a4f
sushantk23 <skurundwade00@gmail.com>
2025-12-12 13:09:33 +0530

    questions

---
+++ assignment/day1/question3.py
@@ -0,0 +1,14 @@
+number1 = input("enter a first number: ")
+number2 = input("enter a  second number: ")
+
+print("\n before swapping")
+print("first number:",number1)
+print("second number:",number2)
+
+temp = number1
+number1 = number2
+number2 = temp
+
+print("\n after swapping")
+print("first number:",number1)
+print("second number:",number2)
---
+++ assignment/day1/question4.py
@@ -0,0 +1,9 @@
+number1 = input("enter a first number: ")
+number2 = input("enter a  second number: ")
+
+sum = int(number1)+int(number2)
+sub = int(number1)-int(number2)
+mul = int(number1)*int(number2)
+div = int(number1)/int(number2)
+print("sum of 2 number is:",sum,"\n sub of 2 number is:",sub,"\n mul of 2 number is:",mul,"\n div of 2 number is:",div)
+
